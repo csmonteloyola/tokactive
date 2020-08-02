8ebc7cab0fa929cb888771ffec13e13bf7737adc
Carole Monteloyola <carole.monteloyola@sap.com>
2020-08-02 10:31:23 +0800

    no message

--- web/index.php
+++ web/index.php
@@ -22,8 +22,9 @@
 
 // Set base path here so this works with symbolic links for multiple installations
 
-require('../vendor/autoload.php');
+
 
 define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']) . '/');
 
+require '../vendor/autoload.php';
 require 'qa-include/qa-index.php';
