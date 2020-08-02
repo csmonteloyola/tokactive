03488d301a3565b58a14e6a05af21d92810bc300
Carole Monteloyola <csmonteloyola@gmail.com>
2020-08-02 13:12:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 
 
-define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']) . '/');
+// define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']) . '/');
 
 require 'vendor/autoload.php';
 require 'qa-include/qa-index.php';
