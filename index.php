1a964f34555a71539650c25ae6c314a311d5ca60
Carole Monteloyola <carole.monteloyola@sap.com>
2020-08-02 10:47:52 +0800

    test

--- index.php
+++ index.php
@@ -26,6 +26,6 @@
 
 define('QA_BASE_DIR', dirname(empty($_SERVER['SCRIPT_FILENAME']) ? __FILE__ : $_SERVER['SCRIPT_FILENAME']) . '/');
 
-require 'qa-include/qa-index.php';
-
 require '../vendor/autoload.php';
+
+require 'qa-include/qa-index.php';
